76fa3cd0060ea1d6b1359b748a7b14c096e07a82
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-11-28 15:30:21 -0800

    Update Ops-301d10 - Challenge 01.sh

--- Class 02/Ops-301d10 - Challenge 01.sh
+++ Class 02/Ops-301d10 - Challenge 01.sh
@@ -7,17 +7,17 @@
 
 # Declaration of variables
 # Define the source file path
-source_file="/var/log/syslog"
+source="/var/log/syslog"
 
 # Generate the date and time suffix using the format %m-%d-%Y
-date_suffix=$(date +"%m-%d-%Y")
+date=$(date +"%m-%d-%Y")
 
 # Defines destination file (the syslog file with the date suffix)
-destination_file="syslog_${date_suffix}.log"
+destination="syslog_${date}.log"
 
 # Declaration of functions
 # Main
 
 # Copy the source file to the destination file
-cp "$source_file" "$destination_file"
+cp "$source" "$destination"
 # End
