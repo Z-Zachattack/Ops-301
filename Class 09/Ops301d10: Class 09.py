b72574c3dde3fcc6c156188f921f56a11e47e101
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-12-07 10:03:29 -0800

    Create Ops301d10: Class 09.py

---
+++ Class 09/Ops301d10: Class 09.py
@@ -0,0 +1,22 @@
+# Script Name:                  Ops-301d10: Challenge 08
+# Author:                       Zachariah Woodbridge
+# Date of latest revision:      07 Dec 2023
+# Purpose:                      Python: Conditional Statements
+
+import random
+# Declaration of variables
+
+a = random.randrange(1, 4)
+b = 2*a
+c = 2+a
+
+# Declaration of functions
+
+# Main
+if b == c:
+    print("a equals 2")
+elif b > c:
+    print("a equals 3")
+else:
+    print("a equals 1")
+# End
