72d57a1785d6c98aeecdfc46a86b3c7209afc7a9
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-12-10 17:57:00 -0800

    Create Ops-301d10 - Class 10.py

---
+++ Ops-301d10 - Class 10.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Ops-301d10 - Class 10
+# Author:                       Zachariah Woodbridge
+# Date of latest revision:      10 Dec 2023
+# Purpose:                      Ops Challenge: Python File Handling
+
+import os
+
+# Declaration of variables
+file_name = "new.txt"
+writefile = open(file_name, "w")
+readfile = open(file_name, "r")
+
+# Declaration of functions
+lines = ["Line 1", "Line 2", "Line 3"]
+
+# Main 
+# Appends three lines to the file
+for line in lines:
+    writefile.write(line + "\n")
+
+# Closes the file after writing
+writefile.close()
+
+# Opens the file again in read mode to read the first line
+readfile = open(file_name, "r")
+first_line = readfile.readline()
+
+# Prints the first line
+print("First line:", first_line)
+
+# Closes the file after reading
+readfile.close()
+
+# Delete the file
+os.remove(file_name)
+
+print("file_name has been deleted")
+# End
