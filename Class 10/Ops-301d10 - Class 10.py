b3adb429f909a5834dbbe6bcc1f87c6621324f92
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-12-10 18:02:01 -0800

    Update Ops-301d10 - Class 10.py

--- Class 10/Ops-301d10 - Class 10.py
+++ Class 10/Ops-301d10 - Class 10.py
@@ -15,25 +15,27 @@ readfile = open(file_name, "r")
 lines = ["Line 1", "Line 2", "Line 3"]
 
 # Main 
-# Appends three lines to the file
+
+# Appends three lines to new.txt
 for line in lines:
     writefile.write(line + "\n")
 
-# Closes the file after writing
+# Closes the file
 writefile.close()
 
-# Opens the file again in read mode to read the first line
+# Opens new.txt in read mode and reads the first line
 readfile = open(file_name, "r")
 first_line = readfile.readline()
 
-# Prints the first line
+# Prints the first line of new.txt
 print("First line:", first_line)
 
-# Closes the file after reading
+# Closes the file
 readfile.close()
 
-# Delete the file
+# Deletes the file
 os.remove(file_name)
 
+# Prints completion
 print(file_name, "has been deleted")
 # End
